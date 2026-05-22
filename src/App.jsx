e073d414d665977725f5119b8d628a215cef2a88
raposinha <elena@raposinha.dev>
2026-05-22 15:15:36 +0200

    remmoves commented code and unused import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css"
 import { BrowserRouter, Route, Routes /* Navigate */ } from "react-router-dom"
 import Home from "./views/Home"
 import TopBar from "./components/TopBar"
-import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -18,55 +17,3 @@ function App() {
 }
 
 export default App
-{
-  /*  <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul> */
-}
